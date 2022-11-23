6097492d420d6a434bd6e700165a2688f5df77e2
shiomi <shiomi@glomouth.co.jp>
2022-11-23 18:32:56 +0900

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,55 @@ import './App.css';
 const p = <div className='box present'></div>
 const b = <div className='box blank'></div>
 const App: React.FC = () => {
-  const [num, setNum] = React.useState<number>(0)
+  const [num, setNum] = React.useState<number>(0) //正しく動いているかの確認用
   const [passedSec, setPassedSec] = React.useState<number>(0);
   const [rows, setRows] = React.useState<JSX.Element[][]>([
-    [b,p,p],
-    [p,p,p],
-    [p,p,p],
-    [p,p,p],
+    [p,b,b,b,b,b,],
+    [b,b,b,b,b,b,],
+    [b,b,b,b,b,b,],
+    [b,b,b,p,b,b,],
   ])
 
   const timerStart = () => {
     let sec = 0;
     setInterval(() => {
-      sec += 1;
+      sec += 5;
       setPassedSec(sec)
-    }, 1000)
+    }, 5000)
   }
+  const moveRight = React.useCallback(() => {
+    const newRows = rows.map((row) => {
+      row.forEach((cell, i) => {
+        const leftCell = row[row.length - 2 - i]
+        if(!leftCell){
+          row[i] = b;
+        }else{
+          if(cell === p){
+            return;
+          }else if(leftCell === p){
+            row[i] = p
+            row[i-1] = b
+          }else{
+            row[i] = b
+          }
+        }
+      })
+      return row
+    })
+    setRows(newRows)
+  },[rows, setRows])
 
   useEffect(() => {
-    setNum(prev => prev++)
-  }, [passedSec])
+    setNum(prev => prev++);
+    // moveRight();
+  }, [passedSec, moveRight])
 
   return(
     <div>
       <button onClick={timerStart}>start</button>
+      {!!passedSec && (
+        <div>経過時間{passedSec}秒</div>
+      )}
       <div id='board'>
         {rows.map((row, i) => (
           <div className='row' key={i}>
