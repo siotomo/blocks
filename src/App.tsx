ee01322468f5baaf611880836b1946fe29ca6419
shiomi <shiomi@glomouth.co.jp>
2022-11-23 15:42:32 +0900

    WIP シンプルにしてsetInterval内部の関数でstateを使うテスト

--- src/App.tsx
+++ src/App.tsx
@@ -4,52 +4,38 @@ import './App.css';
 const p = <div className='box present'></div>
 const b = <div className='box blank'></div>
 const App: React.FC = () => {
+  const [num, setNum] = React.useState<number>(1);
   const [rows, setRows] = React.useState<JSX.Element[][]>([
     [b,p,p],
     [p,p,p],
     [p,p,p],
     [p,p,p],
   ])
-  
-  // 全然思ったように動かない...
-  React.useEffect(() => {
-    setInterval(()=>{
-      let prevRow: JSX.Element[];
-      const newRows = rows.map((row, i) => {
-        if(i === 0){
-          prevRow = row
-          return [b,b,b]
-        }else{
-          const tmp = prevRow
-          prevRow = row
-          return tmp
-        }
-      })
-      setRows(newRows)
-    },1000)
-  },[rows,setRows])
+
+
+  const plusAndDisplayNum = React.useCallback(() => {
+    console.log(num);
+    setNum(num+1);
+  },[num, setNum])
+
+  const timerStart = () => {
+    setInterval(() => {
+      plusAndDisplayNum()
+    }, 1000)
+  }
 
   return(
-    <div id='board'>
-      {rows.map((row, i) => (
-        <div className='row' key={i}>
-          {row.map((box, j) => (
-            <div key={j}>{box}</div>
-          ))}
-        </div>
-      ))}
-      {/* <div className='row'>
-        {p}{b}{b}
+    <div>
+      <button onClick={timerStart}>start</button>
+      <div id='board'>
+        {rows.map((row, i) => (
+          <div className='row' key={i}>
+            {row.map((box, j) => (
+              <div key={j}>{box}</div>
+              ))}
+          </div>
+        ))}
       </div>
-      <div className='row'>
-        {b}{b}{b}
-      </div>
-      <div className='row'>
-        {b}{b}{b}
-      </div>
-      <div className='row'>
-        {b}{b}{b}
-      </div> */}
     </div>
   )
 }
